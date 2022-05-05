f45df610eab04d377cedb32c3bdee756ad62d90d
ludicsa <57569318+ludicsa@users.noreply.github.com>
2022-05-05 12:28:32 -0300

    services.py

---
+++ app/services.py
@@ -0,0 +1,21 @@
+import os 
+
+class Services:
+
+
+    def env_var_list():
+        return os.popen('env').read()
+
+
+
+    def create_env_vars(env_var_name, env_var_content):
+        var = os.environ[str(env_var_name)] = str(env_var_content)
+
+        os.popen('export ' + var)
+
+        return var
+
+
+
+    def list_running_software():
+        return os.popen('ps aux').read()
